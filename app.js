87c118c166dbda457722d4674454132a6152c2fc
leejunghyeok <wndgur76@naver.com>
2022-09-03 22:40:19 +0900

    post Test

--- app.js
+++ app.js
@@ -1,15 +1,68 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
-app.use('/home', (req, res, next) => {
-  const cages = [
-    { id: 1, creatureId: 1, width: 30, depth: 30, height: 45 },
-    { id: 2, creatureId: 2, width: 30, depth: 45, height: 30 },
-    { id: 3, creatureId: 3, width: 30, depth: 30, height: 30 },
-    { id: 4, creatureId: 4, width: 45, depth: 45, height: 30 },
-  ]
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: 0,
+      title: "First server-side post",
+      content: "This is coming from the server",
+      likes: 0,
+      views: 0,
+      commentIds: [],
+      authorId: 0,
+      date: '22.12.03'
+    },
+    {
+      id: 1,
+      title: "Second server-side post",
+      content: "This is coming from the server",
+      likes: 2,
+      views: 5,
+      commentIds: [],
+      authorId: 12312,
+      date: '22.06.11'
+    },
+    {
+      id: 2,
+      title: "Third server-side post",
+      content: "This is coming from the server",
+      likes: 52,
+      views: 11,
+      commentIds: [],
+      authorId: 0,
+      date: '22.02.23'
+    }
+  ];
   res.status(200).json({
-    message: 'Post fetched successfully!',
-    cages: cages
+    message: "Posts fetched successfully!",
+    posts: posts
   });
 });
 
