56ae6ee5287c964074089044394aa852d9cfe18b
leejunghyeok <wndgur76@naver.com>
2022-09-25 17:59:14 +0900

    test

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@ const commentSchema = mongoose.Schema({
   postId: {type: mongoose.Schema.Types.ObjectId, ref: "Post", required:true},
   authorId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required:true},
   authorName: {type: String, required:true},
-  content: {type: String, required:false},
+  content: {type: String, required:true},
   likes: {type: Number, required:true},
 }, {
   timestamps: true,
